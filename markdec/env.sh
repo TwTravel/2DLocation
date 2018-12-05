bf08e0a43b76e63ea88ded4b1b01ee27186fe507
twtravel@126.com <twtravel@126.com>
2018-12-05 20:57:25 +0800

    add files

--- markdec/env.sh
+++ markdec/env.sh
@@ -1,2 +1,2 @@
-export LD_LIBRARY_PATH=/disk2/OpenCvDev/opencv/opencv_contrib-4.0.0/build/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/data/tzwang/opencv_dev/opencv_contrib-4.0.0/build/lib:$LD_LIBRARY_PATH
 
